eff72a57b79ef75eb4ea4d807a564993ada8f594
Nur Alam <54984550+nuralam24@users.noreply.github.com>
2019-12-01 03:00:38 +0600

    Create main.js

---
+++ algorithms/main.js
@@ -0,0 +1,25 @@
+//var input = readline()
+
+var T = readline().split(' ').map(x => parseInt(x)); // total array
+var K = parseInt(readline());  // find value
+print(binarySearch(T, K));
+
+
+function binarySearch(arr, elem) {
+    var start = 0;
+    var end = arr.length - 1;
+    var middle = Math.floor((start + end) / 2);
+
+    while(arr[middle] !== elem && start <= end) {
+        if(elem < arr[middle]) {
+            end = middle - 1;
+        }
+        else {
+            start = middle + 1;
+            middle = Math.floor((start + end) / 2);
+        }
+    }
+    return arr[middle] === elem  ?  middle  :  -1;
+}
+
+//

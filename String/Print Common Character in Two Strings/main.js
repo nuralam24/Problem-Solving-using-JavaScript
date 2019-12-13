346f8b98665d2106d77ffaa1d316f6536c100f81
Nur Alam <54984550+nuralam24@users.noreply.github.com>
2019-12-13 19:48:45 +0600

    Update main.js

--- String/Print Common Character in Two Strings/main.js
+++ String/Print Common Character in Two Strings/main.js
@@ -3,6 +3,24 @@
 var str1 = readline();
 var str2 = readline();
 
+var result = [];
+for (var i = 0; i < str1.length; i++) {
+  if (str2.indexOf(str1[i]) !== -1 && result.indexOf(str1[i]) === -1) {
+    result.push(str1[i]);
+  }
+}
+var L = result.join('').length;
+print(L === 0 ? -1 : L +'\n'+ result.join(''));
+
+
+
+// ********************  Alternative Solution  ********************* //
+
+//var input = readline();
+
+var str1 = readline();
+var str2 = readline();
+
 var one = str1.replace(/ /g, '');
 var two = str2.replace(/ /g, '');
 var result = [];
